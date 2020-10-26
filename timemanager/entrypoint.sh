012b0eda469b73af8c8ecfa7e720dd3c942c6b3d
Lucas Boisbourdin <lucas.boisbourdin@epitech.eu>
2020-10-26 23:44:51 +0100

    Docker Images working

--- timemanager/entrypoint.sh
+++ timemanager/entrypoint.sh
@@ -9,13 +9,16 @@ do
   echo "$(date) - waiting for database to start"
   sleep 2
 done
-
+exec mix ecto.drop
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE does not exist. Creating..."
   createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
   mix ecto.migrate
-  mix run priv/repo/seeds.exs
+  mix run priv/repo/seeds/roles_seeds.exs
+  mix run priv/repo/seeds/users_seeds.exs
+  mix run priv/repo/seeds/clocks_seeds.exs
+  mix run priv/repo/seeds/workingtimes_seeds.exs
   echo "Database $PGDATABASE created."
 fi
 
