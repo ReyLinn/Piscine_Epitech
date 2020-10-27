1a351f1c4559097ede192afbe6d923942f276793
Lucas Boisbourdin <lucas.boisbourdin@epitech.eu>
2020-10-27 19:05:59 +0100

    Jsais pu

--- config/test.exs
+++ config/test.exs
@@ -6,12 +6,12 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :timemanager, TimeManager.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "timemanager_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool: Ecto.Adapters.SQL.Sandbox
+username: "postgres",
+password: "postgres",
+database: "timemanager_dev",
+hostname: "localhost",
+show_sensitive_data_on_connection_error: true,
+pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
