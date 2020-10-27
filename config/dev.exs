f8466721ac07bfffab7ac0a2f0c29b5f93491fbd
Lucas Boisbourdin <lucas.boisbourdin@epitech.eu>
2020-10-27 13:29:55 +0100

    Test ids schemas

--- config/dev.exs
+++ config/dev.exs
@@ -2,15 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :timemanager, TimeManager.Repo,
-  # username: "postgres",
-  # password: "postgres",
-  # database: "timemanager_dev",
-  # hostname: "localhost",
-  username: System.get_env("PGUSER"),
-  password: System.get_env("PGPASSWORD"),
-  database: System.get_env("PGDATABASE"),
-  hostname: System.get_env("PGHOST"),
-  port: String.to_integer(System.get_env("PGPORT")),
+  username: "postgres",
+  password: "postgres",
+  database: "timemanager_dev",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- lib/timemanager/accounts/user.ex
+++ lib/timemanager/accounts/user.ex
@@ -2,6 +2,8 @@ defmodule TimeManager.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "users" do
     field :username, :string
     field :email, :string
