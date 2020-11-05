97d744c427a5a5349ad972d82fd602ead40ce47d
Lucas Boisbourdin <lucas.boisbourdin@epitech.eu>
2020-11-05 18:39:51 +0100

    Test https

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,10 @@ config :timemanager, TimeManager.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :timemanager, TimeManagerWeb.Endpoint,
-  http: [port: 4000],
+  #http: [port: 4000],
+  https: [
+          port: 4000
+        ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
